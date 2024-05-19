c8c5c9b317a2906ec871dd4b0ac5b0338aa1fb83
Francesco Dorati <fd.francescodorati@gmail.com>
2024-05-19 20:00:12 +0200

    .

--- motor_controller.ino
+++ motor_controller.ino
@@ -13,11 +13,20 @@
 #define COUNTS_PER_REV 1500
 #define WHEEL_RADIUS 3.4 // cm
 #define WHEEL_DISTANCE 24 // cm
+#define CONTROLLER_FREQ 50 // Hz
 
 
 #include "Arduino.h"
 #include <string.h>
 #include "MotorController.h"
+
+struct state {
+    int x;
+    int y;
+    int theta;
+    state(): x(0), y(0), theta(0) {};
+    state(int x, int y, int t): x(x), y(y), theta(t) {};
+};
 
 MotorController motor_left(PWM_ML, IN1_ML, IN2_ML, ENCA_ML, ENCB_ML, COUNTS_PER_REV, true);
 MotorController motor_right(PWM_MR, IN1_MR, IN2_MR, ENCA_MR, ENCB_MR, COUNTS_PER_REV, false);
@@ -46,5 +55,34 @@ void loop() {
   Serial.print(" ");
   Serial.println(actual_rpm_right);
 
-  delay(20);
+  delay(1000/CONTROLLER_FREQ);
+}
+
+state BaseController::update_state(float rpm_right, float rpm_left) {
+    float speed_right, speed_left;
+    float robot_linear_velocity, robot_angular_velocity;
+    unsigned long delta_t;
+
+    if (_odometry_prev_time == 0) {
+        _odometry_prev_time = millis();
+        return _state;
+    }
+
+    // calculate delta time
+    delta_t = millis() - _odometry_prev_time;
+
+    // calculate speeds
+    speed_left = rps_left * _wheel_radius;
+    speed_right = rps_right * _wheel_radius;
+    
+    // calculate robot velocities
+    robot_linear_velocity = (speed_right + speed_left) / 2;
+    robot_angular_velocity = (speed_right - speed_left) / _wheel_distance;
+    state new_state = state(
+        _state.x + robot_linear_velocity * cos(_state.theta) * delta_t,
+        _state.y + robot_linear_velocity * sin(_state.theta) * delta_t,
+        _state.theta + robot_angular_velocity * delta_t
+    );
+
+    return new_state;
 }
