44201e4bc64eb3ed8a4449fb0f4137ad99e24f9c
Francesco Dorati <fd.francescodorati@gmail.com>
2024-09-23 19:20:28 +0200

    correct motor pins

--- src/arduino_nano/main.ino
+++ src/arduino_nano/main.ino
@@ -16,9 +16,11 @@
 int pow_l = 0, pow_r = 0;
 void setup() {
     Serial.begin(9600);
+    // Left Motor
     pinMode(ENA, OUTPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
+    // Right Motor
     pinMode(ENB, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
@@ -37,12 +39,15 @@ void loop() {
     }
 
     // check max/min values
+    // left
     if (abs(pow_l) > MAX_POW) pow_l = (pow_l > 0) ? MAX_POW : -MAX_POW;
-    if (abs(pow_r) > MAX_POW) pow_r = (pow_r > 0) ? MAX_POW : -MAX_POW;
     if (abs(pow_l) < MIN_POW) pow_l = 0;
+    // right
+    if (abs(pow_r) > MAX_POW) pow_r = (pow_r > 0) ? MAX_POW : -MAX_POW;
     if (abs(pow_r) < MIN_POW) pow_r = 0;
 
     // update motors
+    // left
     if (pow_l == 0){
         analogWrite(ENA, 0);
         digitalWrite(IN1, LOW);
@@ -52,14 +57,15 @@ void loop() {
         digitalWrite(IN1, (pow_l > 0) ? HIGH : LOW);
         digitalWrite(IN2, (pow_l > 0) ? LOW : HIGH);
     }
+    // right
     if (pow_r == 0){
-        analogWrite(ENA, 0);
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
+        analogWrite(ENB, 0);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
     } else{
-        analogWrite(ENA, abs(pow_r));
-        digitalWrite(IN1, (pow_r > 0) ? HIGH : LOW);
-        digitalWrite(IN2, (pow_r > 0) ? LOW : HIGH);
+        analogWrite(ENB, abs(pow_r));
+        digitalWrite(IN3, (pow_r > 0) ? HIGH : LOW);
+        digitalWrite(IN4, (pow_r > 0) ? LOW : HIGH);
     }
 
     delay(1000/UPDATE_FREQ);
