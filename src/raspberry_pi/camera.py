ce71126410bc2cab4862c94185b0e5d2ec68d366
Francesco Dorati <fd.francescodorati@gmail.com>
2024-10-11 13:33:52 +0200

    ii

--- src/raspberry_pi/camera.py
+++ src/raspberry_pi/camera.py
@@ -12,14 +12,16 @@ class CameraTransmitter:
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
     def send_frame(self):
+        print("UPDATING CAMERA")
         if self.client == None:
             try:
                 _, addr = self.socket.recvfrom(32)
                 self.client = (addr[0], int(addr[1]))
             except BlockingIOError:
                 return
-
+        print("SENDING FRAME")
         ret, frame = self.camera.read()
+        print(ret)
         if ret:
             frame = cv2.rotate(frame, cv2.ROTATE_180)
             _, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
