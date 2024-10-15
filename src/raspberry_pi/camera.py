2686ea57df39cbbd13df866da620bc60ae21612c
Francesco Dorati <fd.francescodorati@gmail.com>
2024-10-15 22:35:31 +0200

    ok

--- src/raspberry_pi/camera.py
+++ src/raspberry_pi/camera.py
@@ -27,7 +27,7 @@ class CameraTransmitter:
             dt_rotate = (time.time() - t_rotate)*1000
 
             t_encode = time.time()
-            _, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            _, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
             dt_encode = (time.time() - t_encode)*1000
 
             t_dumps = time.time()
