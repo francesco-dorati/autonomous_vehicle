46dc7f7f5b31a8563e9d53b074e984ad1bd7290f
Francesco Dorati <fd.francescodorati@gmail.com>
2024-10-26 17:29:25 +0200

    print scan

--- src/raspberry_pi/lidar_test.py
+++ src/raspberry_pi/lidar_test.py
@@ -1,17 +1,24 @@
 from lidar import Lidar
 import time
 
+def print_scan(scan):
+    print("SCAN:")
+    for i in range(0, len(scan)):
+        angle = scan[i][0]
+        distance = scan[i][1]
+        print(f"a: {angle} °,\r d: {distance} mm")
+    print("\n\n")
+    
 l = Lidar()
 l.start_scan()
 
 time.sleep(5)
-print(l.get_scan())
-time.sleep(5)
-print(l.get_scan())
-time.sleep(5)
-print(l.get_scan())
-time.sleep(5)
-print(l.get_scan())
-l.stop_scan()
 
+time.sleep(5)
+print_scan(l.get_scan())
+time.sleep(5)
+print_scan(l.get_scan())
+time.sleep(5)
+print_scan(l.get_scan())
+l.stop_scan()
         
