0686f9e139d6403a7a5c1b1248d82758fda5f969
Francesco Dorati <fd.francescodorati@gmail.com>
2025-02-12 14:00:59 +0100

    ok

--- src/base_station/views/display_frame.py
+++ src/base_station/views/display_frame.py
@@ -6,7 +6,7 @@ class DisplayFrame(tk.Canvas):
     def __init__(self, parent, controller):
         super().__init__(parent)
         total_size = 550
-        self.grid_size = 100
+        self.grid_size = 50
         self.cell_size = lambda: total_size // self.grid_size
         real_total_size = self.cell_size() * self.grid_size
         self.config(borderwidth=2, relief='raised', bg='lightgray', width=real_total_size, height=real_total_size)
@@ -14,34 +14,39 @@ class DisplayFrame(tk.Canvas):
         self.controller = controller
 
     def show(self):
+        print("SHOWING DISPLAY")
         self.pack(side='top', fill='none', expand=False, pady=10)
-        # self.update(None, [(30, 30), [31, 30]], None)
+        # self.update(self.grid_size, None, None, None)
         
-    
     def update(self, size: int, 
                 grid: List[List[int]], 
                 lidar_points: List[Tuple[int, int]], 
                 robot_pos: Tuple[int, int, int]):
+        print("UPDATING DISPLAY")
         self.delete("all")
         self.grid_size = size
-        for row in range(self.grid_size):
-            for col in range(self.grid_size):
-                x1, y1 = col * self.cell_size(), row * self.cell_size()
-                x2, y2 = x1 + self.cell_size(), y1 + self.cell_size()
-                color = "black"
-                if grid:
-                    v = grid[row][col]
-                    if v == -1:
-                        color = "gray"
-                    elif v == 0:
-                        color = "white"
-                    elif v == 1:
-                        color = "black"
-                self.create_rectangle(y1, x1, y2, x2, fill=color, outline="")
+        if grid:
+            for row in range(self.grid_size):
+                for col in range(self.grid_size):
+                    x1, y1 = col * self.cell_size(), row * self.cell_size()
+                    x2, y2 = x1 + self.cell_size(), y1 + self.cell_size()
+                    color = "black"
+                    if grid:
+                        v = grid[row][col]
+                        if v == -1:
+                            color = "gray"
+                        elif v == 0:
+                            color = "white"
+                        elif v == 1:
+                            color = "black"
+                    self.create_rectangle(y1, x1, y2, x2, fill=color, outline="")
 
         if lidar_points:
             for x, y in lidar_points:
-                x1, y1 = x * self.cell_size(), y * self.cell_size()
+                # x = -x # Invert x-axis
+                reversed_x = self.grid_size - x - 1
+                reversed_y = self.grid_size - y - 1
+                x1, y1 = reversed_x * self.cell_size(), reversed_y * self.cell_size()
                 x2, y2 = x1 + self.cell_size(), y1 + self.cell_size()
                 self.create_rectangle(y1, x1, y2, x2, fill="red", outline="")
 
